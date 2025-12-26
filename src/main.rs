7bee6e68326b436259f68f8aa9be11bfac00c82f
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2025-12-26 14:59:25 -0800

    UART Driver + Entry Point + Stack Impl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{arch::global_asm, panic::PanicInfo};
+
+global_asm!(include_str!("entry.S"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -9,6 +11,12 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn start() -> ! {
+    let uart_base_address = 0x1000_0000 as *mut u8;
+
+    unsafe {
+        uart_base_address.add(0).write_volatile(b'a');
+        uart_base_address.add(0).write_volatile(b'\n');
+    }
     loop {}
 }
